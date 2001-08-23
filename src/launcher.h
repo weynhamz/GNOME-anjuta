4fe26b2dc54697a5b5aa20aea66ef15fb1112a2a
Naba Kumar <kh_naba@users.sourceforge.net>
2001-08-23 09:52:37 +0000

    corrected >>>>>>> Config management.
    2001-08-22  Naba Kumar <kh_naba@users.sourceforge.net>
    
    	* HACKING: corrected >>>>>>> Config management.
    
    	* HACKING: Added many things.
    
    	* src/launcher.h: Added some comments.

--- src/launcher.h
+++ src/launcher.h
@@ -21,21 +21,32 @@
 
 #include <gnome.h>
 
+/* This is only called once, before anything starts */
 void launcher_init(void);
 
+/* Get the pid of the currently executing child */
 pid_t launcher_get_child_pid(void);
 
+/* Reset the launcher. This will terminate the current job */
 void launcher_reset(void);
 
+/* Send a signal to the currently executing process */
 void launcher_signal(int);
 
+/* Checks if there is a job being executed */
 gboolean launcher_is_busy(void);
 
+/* Start a job. */
+/* command_str is the command to be executed */
+/* stdout_func is the callback to be called when chars are received in STDOUT from the process */
+/* stderr_func is the callback to be called when chars are received in STDERR from the process */
+/* commnad_terminated is the callback to be called when the job is completed or terminated */
 gboolean launcher_execute(gchar* command_str,
                                void (*stdout_func)(gchar*),
                                void (*stderr_func)(gchar*),
                                void (*command_terminated)(gint status, time_t time));
 
+/* sends the given string to the STDIN of the process */
 void launcher_send_stdin(gchar* input_str);
 
 #endif
