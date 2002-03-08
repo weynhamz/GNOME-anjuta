ba51ab84c0b18a51507013abd0db20f5f7d4d07b
Naba Kumar <naba@src.gnome.org>
2002-03-08 06:25:44 +0000

    CVSdgets/gnomedirlist.c widgets/gnomedirlist.h widgets/gnomefilelist.c
    CVSdgets/gnomedirlist.c
    	widgets/gnomedirlist.h
    	widgets/gnomefilelist.c
    	widgets/gnomefilelist.h:
    
    	Added GPL headings in the file.
    : ----------------------------------------------------------------------

--- widgets/gnomedirlist.h
+++ widgets/gnomedirlist.h
@@ -1,20 +1,25 @@
-/*  GtkDirList
-* This is my first GtkWidget
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU Library General Public License for more details.
-*
-*  You should have received a copy of the GNU Library General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*/
+/*
+ * gnomedirlist.h Copyright (C) 
+ *   Chris Phelps <chicane@reninet.com>,
+ *   Naba kumar <kh_naba@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59 
+ * Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ * 
+ * Original author: Chris Phelps <chicane@reninet.com>
+ * Widget rewrote: Naba Kumar <kh_naba@users.sourceforge.net>
+ */
 
 #include <stdio.h>
 #include <gnome.h>
--- widgets/gnomefilelist.h
+++ widgets/gnomefilelist.h
@@ -1,20 +1,29 @@
-/*  GtkFileList
-*   This is my second GtkWidget
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU Library General Public License for more details.
-*
-*  You should have received a copy of the GNU Library General Public License
-*  along with this program; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*/
+/*
+ * gnomefilelist.h Copyright (C) 
+ *   Chris Phelps <chicane@reninet.com>,
+ *   Naba kumar <kh_naba@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59 
+ * Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ * 
+ * Original author: Chris Phelps <chicane@reninet.com>
+ * Widget rewrote: Naba Kumar <kh_naba@users.sourceforge.net>
+ * Contributions:
+ *     Philip Van Hoof <freax@pandora.be>
+ *     Dan Elphick <dre00r@ecs.soton.ac.uk>
+ *     Stephane Demurget  <demurgets@free.fr>
+ */
 
 #include <stdio.h>
 #include <gnome.h>
