e7c6bd5c0b036978e0ca688266b4ab4ed03afc3a
Jean-Noel Guiheneuf <jnoel@src.gnome.org>
2005-02-15 13:48:15 +0000

    Added new project property ("iconfile")
    Added new project property ("iconfile")

--- plugins/project-wizard/property.c
+++ plugins/project-wizard/property.c
@@ -76,7 +76,8 @@ static const gchar* NPWPropertyTypeString[] = {"hidden",
 					        "string",
 						"list",
 						"directory",
-						"file"};
+						"file",
+						"iconfile"};
 
 /* Property object
  *---------------------------------------------------------------------------*/
@@ -247,6 +248,10 @@ npw_property_create_widget (NPWProperty* this)
 		gnome_file_entry_set_directory_entry (GNOME_FILE_ENTRY (entry), FALSE);
 		if (value) gnome_file_entry_set_filename (GNOME_FILE_ENTRY (entry), value);
 		break;
+	case NPW_ICONFILE_PROPERTY:
+		entry = gnome_icon_entry_new("icon_choice", _("Icone choice"));
+		if (value) gnome_icon_entry_set_filename (GNOME_ICON_ENTRY (entry), value);
+		break;
 	case NPW_LIST_PROPERTY:
 	{
 		GSList* node;
@@ -328,6 +333,9 @@ npw_property_set_value_from_widget (NPWProperty* this, NPWValueTag tag)
 	case NPW_DIRECTORY_PROPERTY:
 	case NPW_FILE_PROPERTY:
 		value = gnome_file_entry_get_full_path (GNOME_FILE_ENTRY (this->widget), FALSE);
+		break;
+	case NPW_ICONFILE_PROPERTY:
+		value = gnome_icon_entry_get_filename(GNOME_ICON_ENTRY(this->widget));
 		break;
 	case NPW_LIST_PROPERTY:
 	{
