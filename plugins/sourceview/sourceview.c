0d430b4347cbaa028db27955b134183940dfef48
Jean-Noel Guiheneuf <jnoel@src.gnome.org>
2006-02-25 12:36:41 +0000

    *** empty log message ***

--- plugins/sourceview/sourceview.c
+++ plugins/sourceview/sourceview.c
@@ -395,9 +395,11 @@ static void ieditor_goto_line(IAnjutaEditor *editor, gint line, GError **e)
 	Sourceview* sv = ANJUTA_SOURCEVIEW(editor);
 	const gfloat LEFT = 0.0;
 	const gfloat CENTER = 0.5;
-	
+
 	gtk_text_buffer_get_iter_at_line(GTK_TEXT_BUFFER(sv->priv->source_buffer),
 									 &iter, line - 1);
+	gtk_text_buffer_place_cursor(GTK_TEXT_BUFFER(sv->priv->source_buffer),
+									 &iter);
 	gtk_text_view_scroll_to_iter(GTK_TEXT_VIEW(sv->priv->source_view),
 											   &iter, 0, TRUE, LEFT, CENTER);
 	gtk_text_buffer_place_cursor(GTK_TEXT_BUFFER(sv->priv->source_buffer),
@@ -414,6 +416,8 @@ static void ieditor_goto_position(IAnjutaEditor *editor, gint position, GError *
 	
 	gtk_text_buffer_get_iter_at_offset(GTK_TEXT_BUFFER(sv->priv->source_buffer),
 									 &iter, position);
+	gtk_text_buffer_place_cursor(GTK_TEXT_BUFFER(sv->priv->source_buffer),
+									 &iter);
 	gtk_text_view_scroll_to_iter(GTK_TEXT_VIEW(sv->priv->source_view),
 											   &iter, 0, TRUE, LEFT, CENTER);
 }
