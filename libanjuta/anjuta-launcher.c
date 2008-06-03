b1f9e4896c970ce70089a8f17e90cbf84edaad90
Sebastien Granjoux <sgranjoux@src.gnome.org>
2008-06-03 17:26:28 +0000

    Fix #536372: Write __MAX_BAUX instead of __MAX_BAUD
    	* libanjuta/anjuta-launcher.c:
    	Fix #536372: Write __MAX_BAUX instead of __MAX_BAUD
    
    svn path=/trunk/; revision=3983

--- libanjuta/anjuta-launcher.c
+++ libanjuta/anjuta-launcher.c
@@ -67,13 +67,13 @@
 #define FILE_INPUT_BUFFER_SIZE		1048576
 #ifndef __MAX_BAUD
 #  if defined(B460800)
-#    define __MAX_BAUX B460800
+#    define __MAX_BAUD B460800
 #  elif defined(B307200)
-#    define __MAX_BAUX B307200
+#    define __MAX_BAUD B307200
 #  elif defined(B256000)
-#    define __MAX_BAUX B256000
+#    define __MAX_BAUD B256000
 #  else
-#    define __MAX_BAUX B230400
+#    define __MAX_BAUD B230400
 #  endif
 #endif
 
