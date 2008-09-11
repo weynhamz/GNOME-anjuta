25acee0aede5fab916b89d7ebae95d371dde02dc
Sebastien Granjoux <sgranjoux@src.gnome.org>
2008-09-11 18:00:02 +0000

    Fix #551574 Project names and descriptions are not translatable
    	* plugins/project-wizard/templates/anjuta-plugin.wiz,
    	plugins/project-wizard/templates/anjuta-plugin-vala.wiz,
    	plugins/project-wizard/templates/cpp.wiz,
    	plugins/project-wizard/templates/django.wiz,
    	plugins/project-wizard/templates/gcj.wiz,
    	plugins/project-wizard/templates/gnome.wiz,
    	plugins/project-wizard/templates/gtk.wiz,
    	plugins/project-wizard/templates/gtkmm.wiz,
    	plugins/project-wizard/templates/java.wiz,
    	plugins/project-wizard/templates/mkfile.wiz,
    	plugins/project-wizard/templates/minimal.wiz,
    	plugins/project-wizard/templates/python.wiz,
    	plugins/project-wizard/templates/sdl.wiz,
    	plugins/project-wizard/templates/terminal.wiz,
    	plugins/project-wizard/templates/wxwin.wiz,
    	plugins/project-wizard/templates/xlib.wiz,
    	plugins/project-wizard/templates/xlib-dock.wiz,
    	plugins/project-wizard/templates/extract-translatable-strings.pl,
    	plugins/project-wizard/templates/translatable-strings.h:
    	Fix #551574 Project names and descriptions are not translatable
    
    svn path=/trunk/; revision=4244

--- plugins/project-wizard/templates/extract-translatable-strings.pl
+++ plugins/project-wizard/templates/extract-translatable-strings.pl
@@ -13,7 +13,19 @@ foreach my $one_file (@files) {
 	next;
     }
     my $content = `cat $one_file`;
-    my @strings = $content =~ /_[\d\w\-\_]+=(\"[^\"]+\")/sgm;
+
+    # Find translatable attribute (name starting with _)
+    my @strings = $content =~ /_[\d\w\-\_]+=\"([^\"]+)\"/sgm;
+    foreach my $one_string (@strings)
+    {
+	$one_string =~ s/&gt;/>/gs;
+	$one_string =~ s/&lt;/</gs;
+	$one_string =~ s/&quot;/\\"/gs;
+	$trans_strings{$one_string} = $one_string;
+    }
+    
+    # Find translatable content (element name starting with _)
+    @strings = $content =~ /<\s*_[^\>]+>([^\<]+)</sgm;
     foreach my $one_string (@strings)
     {
 	$one_string =~ s/&gt;/>/gs;
@@ -24,5 +36,5 @@ foreach my $one_file (@files) {
 }
 foreach my $one_trans_string (sort keys (%trans_strings))
 {
-    print "char *s = N_($one_trans_string);\n";
+    print "char *s = N_(\"$one_trans_string\");\n";
 }
