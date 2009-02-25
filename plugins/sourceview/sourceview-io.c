980da191b92f489b9a43f104711ddb2926e2c048
Yuriy Penkin <yuriy.penkin@gmail.com>
2009-02-25 18:27:51 +0000

    #559806 – Anjuta crashes when closing unsaved file (Patch got lost
    2009-02-25  Yuriy Penkin <yuriy.penkin@gmail.com>
    
    	* plugins/sourceview/sourceview-io.c (on_save_finished):
    	#559806 – Anjuta crashes when closing unsaved file (Patch got lost somewhere)
    
    svn path=/trunk/; revision=4798

--- plugins/sourceview/sourceview-io.c
+++ plugins/sourceview/sourceview-io.c
@@ -243,6 +243,8 @@ on_save_finished (GObject* output_stream, GAsyncResult* result, gpointer data)
 	g_output_stream_write_finish (G_OUTPUT_STREAM(output_stream),
 								  result,
 								  &err);
+	g_free (sio->write_buffer);
+	sio->write_buffer = NULL;
 	if (err)
 	{
 		g_signal_emit_by_name (sio, "save-failed", err);
@@ -252,11 +254,9 @@ on_save_finished (GObject* output_stream, GAsyncResult* result, gpointer data)
 	{
 		set_display_name (sio);
 		g_output_stream_close(G_OUTPUT_STREAM (output_stream), NULL, NULL);
-		g_signal_emit_by_name (sio, "save-finished");
 		setup_monitor (sio);
+		g_signal_emit_by_name (sio, "save-finished");
 	}
-	g_free (sio->write_buffer);
-	sio->write_buffer = NULL;
 	g_object_unref (output_stream);
 }
 
