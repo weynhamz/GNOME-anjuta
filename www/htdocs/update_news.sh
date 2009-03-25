11bed31db946b9daaeb08a5f56efff5fbe165f9c
Johannes Schmid <jhs@src.gnome.org>
2009-03-25 22:32:58 +0000

    OK, retry anjuta homepage with just static content
    svn path=/trunk/; revision=4895

--- www/htdocs/update_news.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Update the news on home page from SourceForge project news
-/usr/bin/wget -q -O anjuta_news.html "http://sourceforge.net/export/projnews.php?group_id=14222&limit=5&flat=0&show_summaries=1"
