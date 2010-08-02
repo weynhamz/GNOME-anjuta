a8c660e50bbedf4a3b1461d9217a4e448d392a55
Fran Diéguez <fran.dieguez@mabishu.com>
2010-08-02 14:23:46 +0200

    Revert "Updated Galician translations"
    This reverts commit e80ab9f6cb6b430ed012b581f481d175ef0b059f.

--- plugins/language-support-js/jsparse.h
+++ plugins/language-support-js/jsparse.h
@@ -1,13 +1,1 @@
-#ifndef JSPARSE_H
-#define JSPARSE_H
-
-#include <glib.h>
-#include "jstypes.h"
-#include "js-node.h"
-#include "js-context.h"
-
-void print_node (JSNode *node, char *pref);
-
-//void print_context (Context *my_cx, const gchar *str);
-
-#endif
+../symbol-db/anjuta-tags/js-parser/jsparse.h
