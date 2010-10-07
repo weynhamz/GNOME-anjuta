76c27fdf221316aa6e38ffac570bd106095d56ae
Javier Jardón <jjardon@gnome.org>
2010-10-07 19:36:52 +0200

    Use autoreconf instead gnome-common

--- autogen.sh
+++ autogen.sh
@@ -4,25 +4,18 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME=anjuta
-
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/autogen.sh \
-  && test -d $srcdir/src \
-  && test -f $srcdir/src/anjuta.c) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
-}
-
 echo "Generating initial interface files"
 sh -c "cd $srcdir/libanjuta/interfaces && \
 perl anjuta-idl-compiler.pl libanjuta && \
 touch iface-built.stamp"
 
-REQUIRED_AUTOMAKE_VERSION=1.11 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+(
+ cd "$srcdir" &&
+ gtkdocize &&
+ gnome-doc-prepare --automake --copy --force &&
+ autopoint --force &&
+ AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
