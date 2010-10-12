339f5852a23b927a8acd260d25750314f3778948
Johannes Schmid <jhs@gnome.org>
2010-10-12 22:38:45 +0200

    Fixed gschema build

--- scripts/build-schemas.mk
+++ scripts/build-schemas.mk
@@ -3,7 +3,10 @@
 prefs_ui_schemas = $(prefs_ui_files:.ui=.gschema.xml)
 
 # gsettings_SCHEMAS is a list of all the schemas you want to install
-gsettings_SCHEMAS = $(prefs_ui_schemas)
+gsettings_SCHEMAS = $(prefs_name).gschema.xml
+
+$(prefs_name).gschema.xml: $(prefs_ui_schemas)
+	mv $< $@
 
 %.gschema.xml: %.ui
 	$(AM_V_GEN)$(top_srcdir)/scripts/builder2schema.pl $< $(prefs_name) $(prefs_keyfile) >> $@
@@ -11,4 +14,4 @@ gsettings_SCHEMAS = $(prefs_ui_schemas)
 # include the appropriate makefile rules for schema handling
 @GSETTINGS_RULES@
 
-CLEANFILES = $(prefs_ui_schemas)
+CLEANFILES = $(prefs_ui_schemas) $(gsettings_SCHEMAS)
