15f8b9fa1a942659e7dd74b05d271290d85f3883
Johannes Schmid <jhs@gnome.org>
2011-11-21 21:01:48 +0100

    git: fix bgo#646695 - Remote needs translator comment

--- plugins/git/git-add-remote-pane.c
+++ plugins/git/git-add-remote-pane.c
@@ -175,6 +175,7 @@ on_add_remote_button_clicked (GtkAction *action, Git *plugin)
 
 	pane = git_add_remote_pane_new (plugin);
 
+	/* Translators: Remote means the git remote repository */
 	anjuta_dock_replace_command_pane (ANJUTA_DOCK (plugin->dock), "AddRemote", 
 	                                  _("Add Remote"), NULL, pane, GDL_DOCK_BOTTOM, NULL, 0,
 	                                  NULL);
