f8cac01e2a10b076f5484275af6332ffc2a5159a
Sasi Bhushan <sasi@swecha.net>
2012-04-17 15:10:48 +0530

    Updated Telugu Translation

--- plugins/language-support-js/js-node.h
+++ plugins/language-support-js/js-node.h
@@ -1,1 +1,92 @@
-../symbol-db/anjuta-tags/js-parser/js-node.h
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+    Copyright (C) 2009 Maxim Ermilov   <zaspire@rambler.ru>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _JS_NODE_H_
+#define _JS_NODE_H_
+
+#include <glib-object.h>
+
+#include "jstypes.h"
+
+G_BEGIN_DECLS
+
+#define JS_TYPE_NODE             (js_node_get_type ())
+#define JS_NODE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), JS_TYPE_NODE, JSNode))
+#define JS_NODE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), JS_TYPE_NODE, JSNodeClass))
+#define JS_IS_NODE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), JS_TYPE_NODE))
+#define JS_IS_NODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), JS_TYPE_NODE))
+#define JS_NODE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), JS_TYPE_NODE, JSNodeClass))
+
+typedef struct _JSNodeClass JSNodeClass;
+typedef struct _JSNode JSNode;
+
+struct _JSNodeClass
+{
+	GObjectClass parent_class;
+};
+
+struct _JSNode
+{
+	GObject parent_instance;
+
+	int        pn_type;
+	int        pn_op;
+	int        pn_arity;
+	JSTokenPos pn_pos;
+	union {
+		struct {                        /* TOK_FUNCTION node */
+			JSNode *body;
+			JSNode *name;
+			JSNode *args;
+		} func;
+		struct {                        /* list of next-linked nodes */
+			JSNode *head;
+		} list;
+		struct {                        /* ternary: if, for(;;), ?: */
+			char dummy;
+		} ternary;
+		struct {                        /* two kids if binary */
+			JSNode *left;
+			JSNode *right;
+		} binary;
+		struct {                        /* one kid if unary */
+			JSNode *kid;
+		} unary;
+		struct {                        /* name, labeled statement, etc. */
+			JSNode *expr;
+			void *name;
+			char isconst;
+		} name;
+		struct {
+			char dummy;
+		} apair;
+    } pn_u;
+    JSNode *pn_next;
+};
+
+GType js_node_get_type (void) G_GNUC_CONST;
+gchar* js_node_get_name (JSNode *node);
+JSNode* js_node_new_from_file (const gchar *name);
+GList* js_node_get_list_member_from_rc (JSNode* node);
+JSNode* js_node_get_member_from_rc (JSNode* node, const gchar *mname);
+GList* js_node_get_lines_missed_semicolon (JSNode *node);
+
+G_END_DECLS
+
+#endif /* _JS_NODE_H_ */
--- plugins/language-support-js/jsparse.c
+++ plugins/language-support-js/jsparse.c
@@ -1,1 +1,146 @@
-../symbol-db/anjuta-tags/js-parser/jsparse.c
+
+#include <stdio.h>
+#include "jstypes.h"
+#include "jsparse.h"
+#include "js-node.h"
+
+void
+print_node (JSNode *node, char *pref)
+{
+	char *pr = g_strconcat(pref, "\t", NULL);
+	JSNode *iter;
+
+	if (node == NULL)
+		return;
+
+	printf ("%s%d\n",pref,node->pn_type);
+	switch (node->pn_arity)
+	{
+		case PN_UNARY:
+			printf ("%sUNARY\n", pref);
+			switch ((JSTokenType)node->pn_type)
+			{
+			case TOK_SEMI:
+				print_node (node->pn_u.unary.kid, pr);
+				break;
+			default:
+				break;
+			}
+			break;
+		case PN_LIST:
+			printf ("%sLIST\n", pref);
+			switch ((JSTokenType)node->pn_type)
+			{
+			case TOK_VAR:
+				for (iter = node->pn_u.list.head; iter != NULL; iter = iter->pn_next)
+				{
+					g_assert (iter->pn_type == TOK_NAME);
+
+					break;
+				}
+			case TOK_LP:
+				print_node (node->pn_u.list.head, pr);
+				int k = 0;
+				for (iter = ((JSNode*)node->pn_u.list.head)->pn_next; iter != NULL; iter = iter->pn_next, k++)
+				{
+					print_node (iter, pr);
+				}
+				break;
+			case TOK_RC:
+				{
+				print_node (node->pn_u.list.head, pr);
+				int k = 0;
+				for (iter = ((JSNode*)node->pn_u.list.head)->pn_next; iter != NULL; iter = iter->pn_next, k++)
+				{
+					print_node (iter, pr);
+				}
+				}
+				break;
+			case TOK_LC:
+				for (iter = node->pn_u.list.head; iter != NULL; iter = iter->pn_next)
+				{
+					print_node (iter, pr);
+				}
+				break;
+			case TOK_NEW:
+				///NOT COMPLETE
+				print_node (node->pn_u.list.head, pr);
+				for (iter = ((JSNode*)node->pn_u.list.head)->pn_next; iter != NULL; iter = iter->pn_next)
+				{
+					print_node (iter, pr);
+				}
+				break;
+			default:
+				break;
+			}
+			break;
+		case PN_BINARY:
+			printf ("%sBINARY\n", pref);
+			switch ((JSTokenType)node->pn_type)
+			{
+				case TOK_ASSIGN:
+					///NOT COMPLETE
+					print_node (node->pn_u.binary.left, pr);
+					print_node (node->pn_u.binary.right, pr);
+
+					break;
+				default:
+					break;
+			}
+			break;
+		case PN_FUNC:
+			printf ("%sFUNC\n", pref);
+			print_node (node->pn_u.func.body, pr);
+			break;
+		case PN_NAME:
+//TODO			printf ("%sNAME\n%s\n", pref, node->pn_u.name.name);
+			printf ("%sNAME\n", pref);
+			print_node (node->pn_u.name.expr, pr);
+/*			JSAtom * atom = node->pn_atom;
+			*code = js_AtomToPrintableString(context, node->pn_atom);			*/
+			break;
+		case PN_NULLARY:
+			printf ("%sNULL\n", pref);
+			switch ((JSTokenType)node->pn_type)
+			{
+				case TOK_STRING:
+//					*code = g_strconcat ("\"", js_AtomToPrintableString(context, node->pn_atom), "\"", NULL);			
+					break;
+				case TOK_NUMBER:
+//					*code = g_new (char, 100);
+//					sprintf (*code, "%lf", node->pn_u.dval);
+					break;
+				default:
+					break;
+			}
+			break;
+		case PN_TERNARY:
+			printf ("%sTERNARY\n", pref);
+/*			print (node->pn_u.ternary.kid1, pr, &in);
+			print (node->pn_u.ternary.kid2, pr, &in);			
+			print (node->pn_u.ternary.kid3, pr, &in);			*/
+			break;
+		
+	}
+	g_free (pr);
+	return;
+}
+/*
+void
+print_context (Context *my_cx, const gchar *str)
+{
+	GList *i;
+	printf ("%s%d-%d\n", str, my_cx->bline, my_cx->eline);
+	for (i = my_cx->local_var; i; i = g_list_next (i))
+	{
+		Var *t = (Var *)i->data;
+		if (!t->name)
+			continue;
+		printf ("%s%s\n", str, t->name);
+	}
+	for (i = g_list_last (my_cx->childs); i; i = g_list_previous (i))
+	{
+		Context *t = (Context *)i->data;
+		print_context (t, g_strdup_printf ("%s%s","\t", str));
+	}
+}*/
